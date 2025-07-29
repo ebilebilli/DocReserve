084e29f89a88f7911e26e940d6481d29c1d17b27
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-29 23:54:39 +0400

    Doctor routers connected in main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+
 from db.base import Base
 from db.session import engine
 from doctors.models import Doctor
+from apis.doctor_apis import router as doctor_router
 
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(doctor_router)
